1a4d75cb892309d4032cdb3a83c535ed37a47aba
Rustem Topuz <topuzr@mail.ru>
2024-05-21 11:14:43 +0300

    First commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let number = [0,1,2,3,4,5,6,7,8,9]
+let a = 0;
+let b = 9;
 
-for (let i = 0; i < number.length; i++) {
-    const n = number [i];
-    console.log(n);
+for (a; a <= b; a++) {
+    console.log(a);
 }
